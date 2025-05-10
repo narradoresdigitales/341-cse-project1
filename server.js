996463b6dc8c2f2fbc1b716364c26558bc362725
sil <narradoresdigitales@gmail.com>
2025-05-10 08:43:29 -0500

    final reivew of codebase for submission.

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
-
 const mongodb = require('./data/database');
 const app = express();
-
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
